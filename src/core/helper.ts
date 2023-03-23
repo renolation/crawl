87ce6eec4c40f3b45bcee5080cfb0ac8af1a9ddc
Phuoc Nguyen <phuoc.nguyen@pamobile.co>
2023-03-23 15:43:34 +0700

    add user agent

--- src/core/helper.ts
+++ src/core/helper.ts
@@ -1,2 +1,9 @@
 
 export const sleep = (ms) => new Promise(r => setTimeout(r, ms));
+
+export const axiosParams = {
+    headers: {
+        "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/111.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    },
+};
--- src/services/truyen-full.service.ts
+++ src/services/truyen-full.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@nestjs/common";
 import {truyenFullURL} from "../core/constants";
-import {sleep} from "../core/helper";
+import {axiosParams, sleep} from "../core/helper";
 import DomParser from 'dom-parser';
 import jsdom from 'jsdom';
 import {HttpService} from '@nestjs/axios';
@@ -8,6 +8,7 @@ import {HttpService} from '@nestjs/axios';
 
  const { JSDOM } = jsdom;
 import axios from "axios";
+import * as cheerio from 'cheerio';
  const parser = new DomParser();
 
 @Injectable()
@@ -20,25 +21,19 @@ export class TruyenFullService{
 
     async crawl1PageOfCategory(category: string, page: number){
         try {
-            const res = await axios.get(`${truyenFullURL}the-loai/${category}/trang-${page}/`);
+            const res = await axios.get(`${truyenFullURL}the-loai/${category}/trang-${page}/`, axiosParams);
             await sleep(1500);
-            const ele = parser.parseFromString(res.data, 'text/html');
-            const dom = new JSDOM(ele.rawHTML);
 
-            const nodeListHTML = dom.window.document.getElementsByClassName('list list-truyen col-xs-12')[0]
-                .getElementsByClassName('row');
+
+            const html = res.data;
+            const $ = cheerio.load(html);
 
             //convert HTML collection to Array
-            const nodeListArray : Array<String> = Array.from(nodeListHTML);
+            const nodeListArray  = Array.from($('.list.list-truyen.col-xs-12').eq(0).find('.row'));
 
             let storyList1Page = [];
             for (const node of nodeListArray) {
-                // const latestChap = node.getElementsByClassName('col-xs-2 text-info')[0].getElementsByTagName('a')[0].innerHTML
-                //   .split('</span>')[2]
-                // @ts-ignore
-                const storyURL = node.getElementsByClassName('truyen-title')[0]
-                    .getElementsByTagName('a')[0]
-                    .getAttribute('href');
+                const storyURL = $(node).find('.truyen-title').eq(0).find('a').eq(0).attr('href');
                 console.log(storyURL);
                 const story = await this.crawlStoryInfo(storyURL);
 
@@ -60,49 +55,66 @@ export class TruyenFullService{
             // await sleep(500);
             // console.log(res);
 
-            const response = await axios.get(`${storyUrl}`);
+            const response = await axios.get(`${storyUrl}`, axiosParams);
             const html = response.data;
+            const $ = cheerio.load(html);
 
-            const ele = parser.parseFromString(html, 'text/html');
-            const dom = new JSDOM(ele.rawHTML);
-
-            const poster = dom.window.document
-                .getElementsByClassName('book')[0]
-                .getElementsByTagName('img')[0].src;
-            const title = dom.window.document
-                .getElementsByClassName('title')[0].innerHTML.normalize();
-            const author = dom.window.document
-                .getElementsByClassName('info')[0]
-                .getElementsByTagName('div')[0]
-                .getElementsByTagName('a')[0].innerHTML.normalize();
-
-            const domHTMLArray = Array.from(dom.window.document
-                .getElementsByClassName('info')[0]
-                .getElementsByTagName('div')[1].getElementsByTagName('a'));
-            const categoryList = domHTMLArray.map(dom => {
-                // @ts-ignore
-                return dom.innerHTML.normalize();
-            })
-
-            const domInfoArray = Array.from(dom.window.document
-                .getElementsByClassName('info')[0]
-                .getElementsByTagName('div'));
-            const status = dom.window.document
-                .getElementsByClassName('info')[0]
-                .getElementsByTagName('div')[domInfoArray.length - 1]
-                .getElementsByTagName('span')[0].innerHTML.normalize();
-
-
-            const description = Array.from(dom.window.document
-                .getElementsByClassName('col-xs-12 col-sm-8 col-md-8 desc')[0]
-                .getElementsByTagName('div'))[3]
-                // @ts-ignore
-                .innerHTML
+            const poster = $('.book').eq(0).find('img').eq(0).attr('src');
+            const title = $('.title').eq(0).text().normalize();
+            const author = $('.info').eq(0).find('div').eq(0).find('a').eq(0).text().normalize();
+            const categories = Array.from($(".info").eq(0).find('div').eq(1).find('a'));
+            const categoryList = categories.map(dom => {
+                return $(dom).text().normalize();
+            });
+            const infoArray = Array.from($('.info').eq(0).find('div'));
+            const status = $('.info').eq(0).find('div').eq(infoArray.length-1).find('span').eq(0).text().normalize();
+            const description = $('.col-xs-12.col-sm-8.col-md-8.desc').eq(0)
+                .find('div')
+                .eq(3)
+                .html()
                 .replace(/<b>|<\/b>/g, '')
                 .replace(/<i>|<\/i>/g, '')
                 .replace(/&nbsp;/g, ' ')
                 .normalize()
                 .split('<br>');
+
+            // const poster = dom.window.document
+            //     .getElementsByClassName('book')[0]
+            //     .getElementsByTagName('img')[0].src;
+
+            // const title = dom.window.document
+            //     .getElementsByClassName('title')[0].innerHTML.normalize();
+
+            // const author1 = dom.window.document
+            //     .getElementsByClassName('info')[0]
+            //     .getElementsByTagName('div')[0]
+            //     .getElementsByTagName('a')[0].innerHTML.normalize();
+
+
+
+
+            // const domInfoArray = Array.from(dom.window.document
+            //     .getElementsByClassName('info')[0]
+            //     .getElementsByTagName('div'));
+
+
+            // const status1 = dom.window.document
+            //     .getElementsByClassName('info')[0]
+            //     .getElementsByTagName('div')[domInfoArray.length - 1]
+            //     .getElementsByTagName('span')[0].innerHTML.normalize();
+
+
+            // const description1 = Array.from(dom.window.document
+            //     .getElementsByClassName('col-xs-12 col-sm-8 col-md-8 desc')[0]
+            //     .getElementsByTagName('div'))[3]
+            //     // @ts-ignore
+            //     .innerHTML
+            //     .replace(/<b>|<\/b>/g, '')
+            //     .replace(/<i>|<\/i>/g, '')
+            //     .replace(/&nbsp;/g, ' ')
+            //     .normalize()
+            //     .split('<br>');
+
 
             return ({
                 title: title,
